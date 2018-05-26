7a6be134589fa49e52e9774651751aa90136e528
Agustin <agustincharry@gmail.com>
2018-05-26 18:24:35 -0500

    Reorganización git

--- README.md
+++
@@ -1,3 +0,0 @@
-# loginApp
-
-a [Sails](http://sailsjs.org) application
--- api/policies/SEC_sessionPermissions.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res, next) {
-	if (SEC_SecurityService.verifyPermissionsToPath(res, req.path)) {
-		return next();
-	}
-	return res.forbidden('You are not permitted to perform this action.');
-};
